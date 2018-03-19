3d69ef0350626600606b540bb1ed34a3288d1080
Senzho <vijagama@outlook.es>
2018-03-19 17:10:46 -0600

    Base de datos
    Se crea la base de datos Ared, se actualiza el modelo de dominio y la
    plantilla de lista de tareas.

---
+++ Diseño/Base de datos/Ared.sql
@@ -0,0 +1,440 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: Ared
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alumno`
+--
+
+DROP TABLE IF EXISTS `alumno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `alumno` (
+  `idAlumno` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `nombre` varchar(150) DEFAULT NULL,
+  `direccion` text,
+  `correo` varchar(150) DEFAULT NULL,
+  `estado` int(11) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`idAlumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumno`
+--
+
+LOCK TABLES `alumno` WRITE;
+/*!40000 ALTER TABLE `alumno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `alumno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `asistencia`
+--
+
+DROP TABLE IF EXISTS `asistencia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `asistencia` (
+  `idAsistencia` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `idGrupo` int(11) DEFAULT NULL,
+  `idAlumno` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idAsistencia`),
+  KEY `idGrupo` (`idGrupo`),
+  KEY `idAlumno` (`idAlumno`),
+  CONSTRAINT `asistencia_ibfk_1` FOREIGN KEY (`idGrupo`) REFERENCES `grupo` (`idGrupo`),
+  CONSTRAINT `asistencia_ibfk_2` FOREIGN KEY (`idAlumno`) REFERENCES `alumno` (`idAlumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `asistencia`
+--
+
+LOCK TABLES `asistencia` WRITE;
+/*!40000 ALTER TABLE `asistencia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `asistencia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cliente` (
+  `idCliente` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `correo` varchar(150) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `nombre` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dia`
+--
+
+DROP TABLE IF EXISTS `dia`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `dia` (
+  `idDia` int(11) NOT NULL AUTO_INCREMENT,
+  `salon` varchar(50) DEFAULT NULL,
+  `tipo` int(11) DEFAULT NULL,
+  `idTipo` int(11) DEFAULT NULL,
+  `dia` varchar(10) DEFAULT NULL,
+  `horaInicio` varchar(5) DEFAULT NULL,
+  `horaFin` varchar(5) DEFAULT NULL,
+  PRIMARY KEY (`idDia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dia`
+--
+
+LOCK TABLES `dia` WRITE;
+/*!40000 ALTER TABLE `dia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dia` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `egreso`
+--
+
+DROP TABLE IF EXISTS `egreso`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `egreso` (
+  `idEgreso` int(11) NOT NULL AUTO_INCREMENT,
+  `monto` varchar(15) DEFAULT NULL,
+  `descripcion` text,
+  `fecha` date DEFAULT NULL,
+  PRIMARY KEY (`idEgreso`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `egreso`
+--
+
+LOCK TABLES `egreso` WRITE;
+/*!40000 ALTER TABLE `egreso` DISABLE KEYS */;
+/*!40000 ALTER TABLE `egreso` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `gastopromocional`
+--
+
+DROP TABLE IF EXISTS `gastopromocional`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `gastopromocional` (
+  `idGasto` int(11) NOT NULL AUTO_INCREMENT,
+  `fechaInicio` date DEFAULT NULL,
+  `fechaFin` date DEFAULT NULL,
+  `monto` varchar(15) DEFAULT NULL,
+  `url` varchar(200) DEFAULT NULL,
+  `descripcion` text,
+  PRIMARY KEY (`idGasto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gastopromocional`
+--
+
+LOCK TABLES `gastopromocional` WRITE;
+/*!40000 ALTER TABLE `gastopromocional` DISABLE KEYS */;
+/*!40000 ALTER TABLE `gastopromocional` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `grupo`
+--
+
+DROP TABLE IF EXISTS `grupo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `grupo` (
+  `idGrupo` int(11) NOT NULL AUTO_INCREMENT,
+  `danza` varchar(50) DEFAULT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `idProfesor` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idGrupo`),
+  KEY `idProfesor` (`idProfesor`),
+  CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`idProfesor`) REFERENCES `profesor` (`idProfesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grupo`
+--
+
+LOCK TABLES `grupo` WRITE;
+/*!40000 ALTER TABLE `grupo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grupo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inscripcion`
+--
+
+DROP TABLE IF EXISTS `inscripcion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inscripcion` (
+  `idInscripcion` int(11) NOT NULL AUTO_INCREMENT,
+  `idGrupo` int(11) DEFAULT NULL,
+  `idAlumno` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idInscripcion`),
+  KEY `idGrupo` (`idGrupo`),
+  KEY `idAlumno` (`idAlumno`),
+  CONSTRAINT `inscripcion_ibfk_1` FOREIGN KEY (`idGrupo`) REFERENCES `grupo` (`idGrupo`),
+  CONSTRAINT `inscripcion_ibfk_2` FOREIGN KEY (`idAlumno`) REFERENCES `alumno` (`idAlumno`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inscripcion`
+--
+
+LOCK TABLES `inscripcion` WRITE;
+/*!40000 ALTER TABLE `inscripcion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inscripcion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pagoalumno`
+--
+
+DROP TABLE IF EXISTS `pagoalumno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pagoalumno` (
+  `idPago` int(11) NOT NULL AUTO_INCREMENT,
+  `tipoPago` int(11) DEFAULT NULL,
+  `idAlumno` int(11) DEFAULT NULL,
+  `idPromocion` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `monto` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`idPago`),
+  KEY `idAlumno` (`idAlumno`),
+  KEY `idPromocion` (`idPromocion`),
+  CONSTRAINT `pagoalumno_ibfk_1` FOREIGN KEY (`idAlumno`) REFERENCES `alumno` (`idAlumno`),
+  CONSTRAINT `pagoalumno_ibfk_2` FOREIGN KEY (`idPromocion`) REFERENCES `promocion` (`idPromocion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagoalumno`
+--
+
+LOCK TABLES `pagoalumno` WRITE;
+/*!40000 ALTER TABLE `pagoalumno` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagoalumno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pagocliente`
+--
+
+DROP TABLE IF EXISTS `pagocliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pagocliente` (
+  `idPago` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `idCliente` int(11) DEFAULT NULL,
+  `monto` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`idPago`),
+  KEY `idCliente` (`idCliente`),
+  CONSTRAINT `pagocliente_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagocliente`
+--
+
+LOCK TABLES `pagocliente` WRITE;
+/*!40000 ALTER TABLE `pagocliente` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagocliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pagoprofesor`
+--
+
+DROP TABLE IF EXISTS `pagoprofesor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pagoprofesor` (
+  `idPago` int(11) NOT NULL AUTO_INCREMENT,
+  `tipoPago` int(11) DEFAULT NULL,
+  `monto` varchar(15) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `idProfesor` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idPago`),
+  KEY `idProfesor` (`idProfesor`),
+  CONSTRAINT `pagoprofesor_ibfk_1` FOREIGN KEY (`idProfesor`) REFERENCES `profesor` (`idProfesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pagoprofesor`
+--
+
+LOCK TABLES `pagoprofesor` WRITE;
+/*!40000 ALTER TABLE `pagoprofesor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pagoprofesor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profesor`
+--
+
+DROP TABLE IF EXISTS `profesor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profesor` (
+  `idProfesor` int(11) NOT NULL AUTO_INCREMENT,
+  `estado` int(11) DEFAULT NULL,
+  `nombre` varchar(150) DEFAULT NULL,
+  `telefono` varchar(10) DEFAULT NULL,
+  `tipoPago` int(11) DEFAULT NULL,
+  `correo` varchar(150) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `direccion` text,
+  PRIMARY KEY (`idProfesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profesor`
+--
+
+LOCK TABLES `profesor` WRITE;
+/*!40000 ALTER TABLE `profesor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profesor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `promocion`
+--
+
+DROP TABLE IF EXISTS `promocion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `promocion` (
+  `idPromocion` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `descripcion` text,
+  `porcentaje` int(11) DEFAULT NULL,
+  `idProfesor` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idPromocion`),
+  KEY `idProfesor` (`idProfesor`),
+  CONSTRAINT `promocion_ibfk_1` FOREIGN KEY (`idProfesor`) REFERENCES `profesor` (`idProfesor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `promocion`
+--
+
+LOCK TABLES `promocion` WRITE;
+/*!40000 ALTER TABLE `promocion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `promocion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `renta`
+--
+
+DROP TABLE IF EXISTS `renta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `renta` (
+  `idRenta` int(11) NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `idCliente` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idRenta`),
+  KEY `idCliente` (`idCliente`),
+  CONSTRAINT `renta_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `renta`
+--
+
+LOCK TABLES `renta` WRITE;
+/*!40000 ALTER TABLE `renta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `renta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(20) DEFAULT NULL,
+  `contrasena` varchar(20) DEFAULT NULL,
+  `tipoUsuario` int(11) DEFAULT NULL,
+  `idTipoUsuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-19 17:05:56
