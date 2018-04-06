c3cd916769598c387c7827d1f7c9d2d09499158c
Senzho <vijagama@outlook.es>
2018-04-06 01:30:39 -0500

    Corrección plantillas
    Se corrigen los primeros registros de las plantillas de casos de uso y
    lista de tareas. Se agregan dos paneles de la interfaz gráfica
    correspondiente al modulo de grupos.

---
+++ Desarrollo/src/LogicaNegocio/Grupos/PanelAlumnoProfesorController.java
@@ -0,0 +1,12 @@
+package LogicaNegocio.Grupos;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class PanelAlumnoProfesorController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+}
