bb6892e92b94b9dd0b70a254316c08845029b18e
MarioolopezISO <marioolopez21@gmail.com>
2018-04-27 01:57:07 -0500

    Mecanismo para obtener fotos.
    Se implementa el mecanismo para obtener fotos de los usuarios desde
    alguna ruta del sistema.

--- Desarrollo/obtenerguardarfoto/CopiarArchivo.java
+++ Desarrollo/obtenerguardarfoto/CopiarArchivo.java
@@ -9,6 +9,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  *
@@ -16,7 +19,71 @@ import java.io.IOException;
  */
 public class CopiarArchivo {
 
-    public boolean copiar(String origen, String destino) throws IOException {
+    private static String rutaImagen;
+
+    public static Image buscarFoto() {
+        File ArchivoImagen = null;
+        FileChooser buscador = new FileChooser();
+        buscador.setTitle("imagenes");
+        buscador.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("JPG", "*.jpg")
+        );
+        ArchivoImagen = buscador.showOpenDialog(new Stage());
+        String origen = ArchivoImagen.getAbsolutePath();
+        Image imagen = new Image("file:" + origen);
+        setRuta(origen);
+        return imagen;
+    }
+    public static Image obtenerFotoUsuario(String tipoUsuario, int idUsuario){
+        Image imagen = null;
+        if (tipoUsuario.equalsIgnoreCase("alumno")) {
+           imagen = new Image("file:"+"C:\\Ared\\Alumnos\\"+idUsuario+".jpg");
+        } else if (tipoUsuario.equalsIgnoreCase("profesor")) {
+           imagen = new Image("file:"+"C:\\Ared\\Profesores\\"+idUsuario+".jpg");
+        } else if (tipoUsuario.equalsIgnoreCase("cliente")) {
+           imagen = new Image("file:"+"C:\\Ared\\Clientes\\"+idUsuario+".jpg");
+        }
+        return imagen;
+    }
+    private static void setRuta(String ruta) {
+        rutaImagen = ruta;
+    }
+
+    public static String rutaImagen() {
+        return rutaImagen;
+    }
+
+    public static boolean guardar(String tipoUsuario, String rutaOrigen, int idUsuario) throws IOException {
+        boolean guardado = false;
+        File directorio = null;
+        File fotos = new File("C:\\Ared");
+        if(!fotos.exists()){
+            fotos.mkdir();
+        }
+        if (tipoUsuario.equalsIgnoreCase("alumno")) {
+            directorio = new File("C:\\Ared\\Alumnos");
+            if (!directorio.exists()) {
+                directorio.mkdir();
+            }
+            guardado = copiar(rutaOrigen,directorio.getAbsolutePath(), idUsuario);
+        } else if (tipoUsuario.equalsIgnoreCase("profesor")) {
+            directorio = new File("C:\\Ared\\Profesores");
+            if (!directorio.exists()) {
+                directorio.mkdir();
+            }
+            guardado = copiar(rutaOrigen,directorio.getAbsolutePath(), idUsuario);
+
+        } else if (tipoUsuario.equalsIgnoreCase("cliente")) {
+            directorio = new File("C:\\Ared\\Clientes");
+            if (!directorio.exists()) {
+                directorio.mkdir();
+            }
+            guardado = copiar(rutaOrigen,directorio.getAbsolutePath(), idUsuario);
+        }
+        return guardado;
+    }
+
+    private static boolean copiar(String origen, String destino, int idUsuario) throws IOException {
         File archivoOrigen;
         File archivoDestino;
         FileInputStream entrada = null;
@@ -28,7 +95,7 @@ public class CopiarArchivo {
             if (copiado = archivoOrigen.exists()) {
                 if (copiado = archivoOrigen.canRead()) {
                     entrada = new FileInputStream(archivoOrigen);
-                    salida = new FileOutputStream(archivoDestino);
+                    salida = new FileOutputStream(archivoDestino+"\\"+idUsuario+".jpg");
                     int i;
                     while ((i = entrada.read()) != -1) {
                         salida.write(i);
--- Desarrollo/obtenerguardarfoto/VentanaImagenPruebaController.java
+++ Desarrollo/obtenerguardarfoto/VentanaImagenPruebaController.java
@@ -5,7 +5,6 @@ package obtenerguardarfoto;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,11 +12,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 /**
@@ -30,12 +26,11 @@ public class VentanaImagenPruebaController implements Initializable {
     @FXML
     private ImageView imagenUsuario;
     @FXML
-    private Button btnCargarImagen;
-    @FXML
-    private TextField txtTipoUsuario;
-    String idUsuario = "10";
+    int idUsuario = 10;
     String nombre = "Jose";
     private Stage stage;
+    private String origen;
+    private Image foto;
 
     /**
      * Initializes the controller class.
@@ -44,28 +39,16 @@ public class VentanaImagenPruebaController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
-    public void setStage(Stage stage){
+
+    public void setStage(Stage stage) {
         this.stage = stage;
     }
+
+    public void btnGuardar_onClick() {
+    }
+
     public void btnCargarImagen_onClick() {
-        FileChooser buscador = new FileChooser();
-        buscador.setTitle("imagenes");
-        buscador.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("JPG", "*.jpg")
-        );
-        File imagenUno = buscador.showOpenDialog(this.stage);
-        if (imagenUno != null) {
-            String origen = imagenUno.getAbsolutePath();
-            System.out.println(origen);
-            String destino = "C:\\Users\\Desktop\\Desktop\\Fotos\\" + idUsuario +".jpg";
-            try {
-                System.out.println("Proceso de copiar archivo: " + new CopiarArchivo().copiar(origen, destino));
-            } catch (IOException ex) {
-                Logger.getLogger(VentanaImagenPruebaController.class.getName()).log(Level.SEVERE, null, ex);
-            }
-            Image imagen = new Image("file:"+destino);
-            this.imagenUsuario.setImage(imagen);
-        }
+        this.imagenUsuario.setImage(CopiarArchivo.obtenerFotoUsuario("alumno", idUsuario));
     }
 
 }
